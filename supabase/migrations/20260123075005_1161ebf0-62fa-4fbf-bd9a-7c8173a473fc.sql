ff1ff2ca7c525f593ec90211c7c3fb82851bbc3c
AREXANS <89494133+AREXANS@users.noreply.github.com>
2026-02-19 13:35:19 +0700

    Wipe all

--- supabase/migrations/20260123075005_1161ebf0-62fa-4fbf-bd9a-7c8173a473fc.sql
+++
@@ -1,2 +0,0 @@
--- Enable realtime for site_settings table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.site_settings;
--- supabase/migrations/20260124053447_7c243d00-6b38-40e1-a5c9-6bc0ae6f1c9b.sql
+++
@@ -1,2 +0,0 @@
--- Enable realtime for transactions table
-ALTER PUBLICATION supabase_realtime ADD TABLE public.transactions;
